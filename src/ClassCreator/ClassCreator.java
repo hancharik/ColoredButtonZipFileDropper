3a1510222310f45e06adf0187a0940a012e013da
hancharik <mah537@psu.edu>
2015-12-13 13:27:44 -0500

    buttons now show the RGB numbers, in case you like the color

--- src/ClassCreator/ClassCreator.java
+++ src/ClassCreator/ClassCreator.java
@@ -6,12 +6,15 @@
 package ClassCreator;
 
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 /**
  * A simple example showing how to use {@link FileDrop}
@@ -21,7 +24,13 @@ public class ClassCreator {
 
 
      public static ArrayList<String> contents = new ArrayList<String>();
-    
+      public static final javax.swing.JTextArea text = new javax.swing.JTextArea();
+        
+        
+        
+       public static JPanel p = new JPanel(); 
+         public static JButton[] studentButtonArray;
+         public static JPanel colored;
     
     /** Runs a sample program that shows dropped files */
     public static void main( String[] args )
@@ -30,19 +39,17 @@ public class ClassCreator {
         javax.swing.border.TitledBorder dragBorder = new javax.swing.border.TitledBorder( "Drop 'em" );
       
         
-        final javax.swing.JTextArea text = new javax.swing.JTextArea();
+      
+        p.setLayout(new GridLayout(2 ,1));
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        p.add(text);
+        colored = new JPanel();
+        colored.setLayout(new GridLayout(1 ,1));
+        colored.setBackground(Color.red);
+    //  colored = createResultButtons(4);
+        p.add(colored);
         frame.getContentPane().add( 
-            new javax.swing.JScrollPane( text ), 
+            new javax.swing.JScrollPane( p ), 
             java.awt.BorderLayout.CENTER );
         
         new FileDrop( System.out, text, /*dragBorder,*/ new FileDrop.Listener()
@@ -80,18 +87,44 @@ public class ClassCreator {
                         int B= (int)(Math.random( )*256);
                         Color randomColor1 = new Color(13, 222, 61);
                         Color randomColor2 = new Color(0, 0, 0);
+                         Color randomColor3 = new Color(R, G, B);
                       // this sets a new text color for every file drop  
                       text.setForeground(randomColor1);
                        text.setBackground(randomColor2);
                        // text.append( "r " + R + "g " + G + "b " + B + "\n" );    
-                     text.append( "Here's the created class "  + "\n" );    
+                     text.append( "Here's the created class ("  + contents.size() + " students)\n" );    
 
 
                         
                         for(int k = 0; k < contents.size(); k++){
                             
-                           text.append( contents.get(k) + "\n" ); 
-                        }
+                          text.append( contents.get(k) + "\n" ); 
+                       }
+                        
+                        colored.removeAll();
+                        colored.add(createResultButtons(contents.size()));
+                        
+                        //colored.setBackground(randomColor3);//.removeAll();
+                     //   p.repaint();
+                     //colored = createResultButtons(6);   
+                 //colored = createResultButtons(contents.size());
+                  // frame.getContentPane().remove(text);      
+                        
+                   
+                  // p.add(createResultButtons(contents.size()));
+                   
+                   
+                   
+                   
+                      //   frame.getContentPane().add( p , java.awt.BorderLayout.CENTER );
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
                         
                         
                         
@@ -108,6 +141,47 @@ public class ClassCreator {
         frame.setVisible(true);
     }   // end main
 
- 
+    
+   private static JPanel createResultButtons(int numberOfStudents){
+    
+     
+    
+             
+    JPanel p1 = new JPanel(); 
+    int rowNumber = 7;
+    int dividedBySeven = (numberOfStudents/rowNumber) + 1;
+    //p.setLayout(new GridLayout((numberOfStudents/7)+1,7));
+    if(numberOfStudents == 7){
+       p1.setLayout(new GridLayout(1,7));  
+    }else{
+    p1.setLayout(new GridLayout(dividedBySeven ,rowNumber));
+    }
+    studentButtonArray = new JButton[numberOfStudents];
+    for(int i = 0; i < studentButtonArray.length; i++){
+        JButton b = new JButton();
+       // b.setText(contents.get(i));
+        // b.setText("" + i);
+           
+                        int R = (int) (Math.random( )*256);
+                        int G = (int)(Math.random( )*256);
+                        int B= (int)(Math.random( )*256);
+                        Color randomButtonColor = new Color(R, G, B);
+                        b.setText( R + "," + G + "," + B );  // this is informativ if you are interested in a particular color, shos the values
+        b.setBackground(randomButtonColor);
+       // b.addActionListener(new RunConfiguration.StudentButtonListener());
+        studentButtonArray[i] = b;
+        p1.add(b);
+    }
+     
+     
+     return p1;
+                
+              
+ }  // end create result buttons   
+    
+    
+    
+    
+    
 
-}
+} // end
